4a608a3184eb4a674ab7cc2a564e0a23c228b868
Saracier <105667230+Saracier@users.noreply.github.com>
2023-12-14 16:47:09 +0100

    feat: cometitions component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CompetitionsComponent } from './containers/competitions/competitions.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, CompetitionsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/containers/competitions/competitions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competitions',
+  templateUrl: './competitions.component.html',
+  styleUrls: ['./competitions.component.scss'],
+})
+export class CompetitionsComponent {}
